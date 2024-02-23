907512219fd841d0d0892eea5f725e20908c89fc
Rathan-R <rathanvishnu2003@gmail.com>
2024-02-23 12:31:23 +0530

    feat : package added

---
+++ react/igniting-ep2/app.js
@@ -0,0 +1,21 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "head" }, "I am heading"),
+    React.createElement("h2", { id: "head2" }, "I am h2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "head" }, "I am heading"),
+    React.createElement("h2", { id: "head2" }, "I am h2"),
+  ]),
+]);
+console.log(parent); //object--->this object is a react element
+
+/* const h1 = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello world from react!!"
+);
+console.log(h1); */
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
