46af1c3c7e51e92aa8666211e3450c924f3132c7
Rathan-R <rathanvishnu2003@gmail.com>
2024-03-02 17:22:42 +0530

    assigment uploaded

--- react/laying-foundation-ep3/app.js
+++ react/laying-foundation-ep3/app.js
@@ -6,9 +6,27 @@ import ReactDOM from "react-dom/client";
 // console.log(heading);
 
 // jsx==>html-like or xml-like syntax
-const jsxheading = <p id="heading">This is rathan from jsx</p>;
-console.log(jsxheading);
+
+//React Element
+const heading = <h1 id="heading">This is rathan heading from jsx</h1>;
+
+//React functional Component
+const HeadingComponent = () => <h1>this is functional component</h1>;
+const HeadingComponent2 = () => (
+  <div>
+    <p>Element inside component</p>
+    {heading}
+    <p>component inside component</p>
+    <HeadingComponent />
+    <HeadingComponent></HeadingComponent>
+
+    {HeadingComponent()}
+    <h1>this is functional component2</h1>
+  </div>
+);
+
+// const HeadingComponent2 = () => <h1>this is functional component</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxheading);
+root.render(<HeadingComponent2 />);
